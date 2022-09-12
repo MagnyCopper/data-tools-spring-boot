fa2cc854663cc04e5ab895e46fc8a85a817742f9
ChangMing Han <han.changming@qq.com>
2022-09-12 23:10:52 +0800

    init commit

---
+++ src/main/java/io/github/magnycopper/datatools/DataToolsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.github.magnycopper.datatools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataToolsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataToolsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/magnycopper/datatools/DataToolsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.magnycopper.datatools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataToolsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
